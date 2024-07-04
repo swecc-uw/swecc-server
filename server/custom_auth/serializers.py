2040bc5af96383bc2f57823ed234b262e3bde9cb
Shawn Collinge <shawnc6@cs.washington.edu>
2024-07-04 16:38:52 -0700

    Add test front end and authentication

--- server/custom_auth/serializers.py
+++ server/custom_auth/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from auth.views import CreateUserView
+from custom_auth.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView    
 
 urlpatterns = [
