2e9a6a6b684f59d5528723569bfd59f844165c9c
Elijah Melton <elimelt@uw.edu>
2024-08-03 19:41:56 -0700

    added endpoint to get user by id throuigh directory service

--- server/directory/views.py
+++ server/directory/views.py
@@ -3,11 +3,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Q
 from members.models import Member
+import status
 from .serializers import DirectoryMemberSerializer
 
 # TODO: filter fields by isPrivate
 
+<<<<<<< Updated upstream
 class MemberDirectoryView(APIView):
+=======
+class MemberDirectorySearchView(APIView):
+    permission_classes = [IsAuthenticated]
+>>>>>>> Stashed changes
     def get(self, request):
         query = request.query_params.get('q', '')
 
@@ -21,3 +27,16 @@ class MemberDirectoryView(APIView):
 
         serializer = DirectoryMemberSerializer(members, many=True)
         return Response(serializer.data)
+
+class MemberDirectoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # get id from path parameter
+        id = request.query_params.get('id')
+
+        # get Member object with member.user.id == id
+        try:
+            member = Member.objects.get(user__id=id)
+        except Member.DoesNotExist:
+            return Response({"detail": "Member not found."}, status=status.HTTP_404_NOT_FOUND)
