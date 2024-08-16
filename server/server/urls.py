6bc8bd701e00ad3305d2f62fae37105f893857fe
Shawn Collinge <shawnc6@cs.washington.edu>
2024-08-16 14:41:54 -0700

    Update url and workflow

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/auth/', include('custom_auth.urls')),
-    path('api/questions/', include("questions.urls")),
-    path('api/members/', include("members.urls")),
-    path('api/interview/', include("interview.urls")),
-    path('api/directory/', include('directory.urls')),
+    path('auth/', include('custom_auth.urls')),
+    path('questions/', include("questions.urls")),
+    path('members/', include("members.urls")),
+    path('interview/', include("interview.urls")),
+    path('directory/', include('directory.urls')),
 ]
