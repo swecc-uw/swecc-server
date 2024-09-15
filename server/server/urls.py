eba3f2b962e0c2e50d9490c5fc843eef21e8b263
Elijah Melton <elimelt@uw.edu>
2024-09-14 17:54:28 -0700

    remove DEBUG

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import DEBUG
 
 urlpatterns = [
     path('auth/', include('custom_auth.urls')),
@@ -12,13 +11,13 @@ urlpatterns = [
     path('directory/', include('directory.urls')),
 ]
 
-if DEBUG:
-    import logging
-    logger = logging.getLogger(__name__)
 
-    @api_view(['GET'])
-    def health_check(request):
-        logger.info("Health check")
-        return Response({'status': 'ok'})
+import logging
+logger = logging.getLogger(__name__)
 
-    urlpatterns.append(path('health/', health_check))
+@api_view(['GET'])
+def health_check(request):
+    logger.info("Health check")
+    return Response({'status': 'ok'})
+
+urlpatterns.append(path('health/', health_check))
