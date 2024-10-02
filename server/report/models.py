ddc833b0b387f4a21e3e1eb028132ddace5c60d1
hoanghohohaha <hoangng@cs.washington.edu>
2024-10-01 20:23:56 -0700

    report model

--- server/report/models.py
+++ server/report/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Report(models.Model):
     REPORT_TYPE_CHOICES = [
@@ -18,7 +20,7 @@ class Report(models.Model):
     report_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     associated_id = models.ForeignKey('interview.Interview', null=True, blank=True, on_delete=models.CASCADE)
     type = models.CharField(max_length=50, choices=REPORT_TYPE_CHOICES)
-    reporter_member_id = models.UUIDField()
+    reporter_member_id = models.ForeignKey(User, on_delete=models.CASCADE)
     reason = models.TextField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     updated = models.DateTimeField(auto_now=True)
