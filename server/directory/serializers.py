32ec7cd6ebe089b425e630d54bb3d883515c4f35
Elijah Melton <elimelt@uw.edu>
2024-11-17 22:03:02 -0800

    directory nice for admin now

--- server/directory/serializers.py
+++ server/directory/serializers.py
@@ -1,40 +1,61 @@
 from rest_framework import serializers
 from members.models import User
 
-class DirectoryMemberSerializer(serializers.ModelSerializer):
+
+class BaseDirectoryMemberSerializer(serializers.ModelSerializer):
     linkedin = serializers.SerializerMethodField()
     github = serializers.SerializerMethodField()
     leetcode = serializers.SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'major', 'grad_date', 'linkedin', 'github', 'leetcode']
-
     def get_social_field(self, obj, field_name):
         field = getattr(obj, field_name)
-        if isinstance(field, dict) and not field.get('isPrivate', True):
-            return {k: v for k, v in field.items() if k != 'isPrivate'}
+        if isinstance(field, dict) and not field.get("isPrivate", True):
+            return {k: v for k, v in field.items() if k != "isPrivate"}
         return None
 
     def get_linkedin(self, obj):
-        return self.get_social_field(obj, 'linkedin')
+        return self.get_social_field(obj, "linkedin")
 
     def get_github(self, obj):
-        return self.get_social_field(obj, 'github')
+        return self.get_social_field(obj, "github")
 
     def get_leetcode(self, obj):
-        return self.get_social_field(obj, 'leetcode')
+        return self.get_social_field(obj, "leetcode")
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
 
         # remove empty fields
-        fields_to_remove = []
-        for field in self.Meta.fields:
-            if representation.get(field) in [None, {}, [], '']:
-                fields_to_remove.append(field)
-        
+        fields_to_remove = set()
+        for field in representation:
+            if representation.get(field) in [None, ""]:
+                fields_to_remove.add(field)
+
         for field in fields_to_remove:
             representation.pop(field)
 
         return representation
+
+
+class RegularDirectoryMemberSerializer(BaseDirectoryMemberSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "discord_username",
+            "first_name",
+            "last_name",
+            "major",
+            "grad_date",
+            "linkedin",
+            "github",
+            "leetcode",
+        ]
+
+
+class AdminDirectoryMemberSerializer(BaseDirectoryMemberSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
--- server/directory/urls.py
+++ server/directory/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MemberDirectorySearchView, MemberDirectoryView
+from .views import MemberDirectorySearchView, MemberDirectoryView, RecommendedMembersView
 
 urlpatterns = [
     path('search/', MemberDirectorySearchView.as_view(), name='member-directory-search'),
+    path('recommended/', RecommendedMembersView.as_view(), name='recommended-members'),
     path('<int:id>/', MemberDirectoryView.as_view(), name='member-directory'),
 ]
