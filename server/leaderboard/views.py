e8254e56b92edadc180f6be262048793d5d3c0ac
Elijah Melton <98496503+elimelt@users.noreply.github.com>
2024-11-29 06:14:16 +0000

    remove blocking update and comments

--- server/leaderboard/views.py
+++ server/leaderboard/views.py
@@ -18,19 +18,10 @@ class LeetcodeLeaderboardView(generics.ListAPIView):
 
     def get_queryset(self):
         latest_stat = LeetcodeStats.objects.order_by("-last_updated").first()
-
-        # TODO: move to async background process
-        if not latest_stat or timezone.now() - latest_stat.last_updated > timedelta(
-            hours=1
-        ):
-            logger.info("Updating Leetcode stats")
-            call_command("update_leetcode_stats", timeout=2)
-
         order_by = self.request.query_params.get("order_by", "total")
         time_range = self.request.query_params.get("updated_within", None)
 
         queryset = LeetcodeStats.objects.all()
-
         queryset = queryset.annotate(
             completion_rate=ExpressionWrapper(
                 (F("total_solved") * 100.0)
@@ -39,7 +30,6 @@ class LeetcodeLeaderboardView(generics.ListAPIView):
             )
         )
 
-        # time filter
         if time_range:
             try:
                 hours = int(time_range)
@@ -48,7 +38,6 @@ class LeetcodeLeaderboardView(generics.ListAPIView):
             except ValueError:
                 raise ValidationError("updated_within must be a valid number of hours")
 
-        # ordering
         ordering_options = {
             "total": "-total_solved",
             "easy": "-easy_solved",
@@ -77,7 +66,6 @@ class GitHubLeaderboardView(generics.ListAPIView):
 
         queryset = GitHubStats.objects.all()
 
-        # time filter
         if time_range:
             try:
                 hours = int(time_range)
@@ -86,7 +74,6 @@ class GitHubLeaderboardView(generics.ListAPIView):
             except ValueError:
                 raise ValidationError("updated_within must be a valid number of hours")
 
-        # ordering
         ordering_options = {
             "commits": "-total_commits",
             "prs": "-total_prs",
