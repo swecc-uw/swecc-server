adf61c0bd55c444957d20a436960909e509f5477
Bhavik Soni <bhaviksoni921@gmail.com>
2024-12-28 22:21:29 -0800

    utc and key validation

--- server/engagement/models.py
+++ server/engagement/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 from members.models import User
 
 class AttendanceSession(models.Model):
@@ -10,6 +12,23 @@ class AttendanceSession(models.Model):
 
     class Meta:
         indexes = [models.Index(fields=['-expires'])]
+    
+    def clean(self):
+        # Expires must be in UTC
+        if self.expires and self.expires.tzinfo != timezone.utc:
+            self.expires = self.expires.astimezone(timezone.utc)
+
+        # Key must be unique for active sessions
+        if not self.pk:
+            active_session = AttendanceSession.objects.filter(key=self.key, expires__gt=timezone.now()).exists()
+            if active_session:
+                raise ValidationError("Key already exists for an active session")
+
+        super().clean()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def is_active(self):
         """Returns true if the session is still active"""
--- server/engagement/tests.py
+++ server/engagement/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+from datetime import timedelta
+import pytz
 
-# Create your tests here.
+from engagement.models import AttendanceSession
+
+class AttendanceSessionTests(TestCase):
+    def setUp(self):
+        self.future_time = timezone.now() + timedelta(hours=1)
+        self.past_time = timezone.now() - timedelta(hours=1)
+        
+    def test_expires_converts_to_utc(self):
+        # Create session with non-UTC timezone
+        est = pytz.timezone('US/Eastern')
+        est_time = self.future_time.astimezone(est)
+        
+        session = AttendanceSession.objects.create(
+            key="TEST123",
+            title="Test Session",
+            expires=est_time
+        )
+        
+        # Verify timezone is UTC
+        self.assertEqual(session.expires.tzinfo, timezone.utc)
+        # Verify time value is equivalent
+        self.assertEqual(session.expires, est_time.astimezone(timezone.utc))
+
+    def test_unique_key_constraint_active_sessions(self):
+        # Create first session
+        AttendanceSession.objects.create(
+            key="TEST123",
+            title="Test Session 1",
+            expires=self.future_time
+        )
+        
+        # Attempt to create second session with same key
+        with self.assertRaises(ValidationError):
+            AttendanceSession.objects.create(
+                key="TEST123",
+                title="Test Session 2",
+                expires=self.future_time
+            )
+
+    def test_allow_duplicate_key_for_expired_session(self):
+        # Create expired session
+        AttendanceSession.objects.create(
+            key="TEST123",
+            title="Test Session 1",
+            expires=self.past_time
+        )
+        
+        # Should be able to create new session with same key
+        try:
+            AttendanceSession.objects.create(
+                key="TEST123",
+                title="Test Session 2",
+                expires=self.future_time
+            )
+        except ValidationError:
+            self.fail("Should allow duplicate key for expired session")
