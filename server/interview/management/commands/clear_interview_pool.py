9ab74b802a2a87cfead1b453cafe774e21fd5c60
Elijah Melton <elimelt@uw.edu>
2025-01-05 16:57:24 -0800

    clear pool command

---
+++ server/interview/management/commands/clear_interview_pool.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.db.models import Q
+import random
+from interview.models import InterviewPool
+
+class Command(BaseCommand):
+    help = 'View the interview pool (current signups)'
+
+    def handle(self, *args, **options):
+        self.stdout.write(
+          self.style.SUCCESS(
+            "Clearing the interview pool..."
+          )
+        )
+        try:
+          n, d = InterviewPool.objects.all().delete()
+          self.stdout.write(
+            self.style.SUCCESS(
+              f"Interview pool cleared, {n} members removed"
+            )
+          )
+        except Exception as e:
+          self.stdout.write(
+            self.style.ERROR(
+              f"An error occurred: {e}"
+            )
+          )
