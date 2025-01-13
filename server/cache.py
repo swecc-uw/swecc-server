e635f6350a2ea49ee1fd037cd095144e5931605e
Advay Patil <advaypatil27@gmail.com>
2025-01-12 23:12:03 -0800

    Add prefix to reduce collusion

--- server/cache.py
+++ server/cache.py
@@ -14,8 +14,8 @@ class CacheHandler(ABC):
 
 class CachedView(ABC):
     @abstractmethod
-    def generate_key(self, **kwargs):
-        return "-".join(kwargs.values())
+    def generate_key(self, prefix, **kwargs):
+        return "-".join([prefix] + kwargs.values())
 
 
 class DjangoCacheHandler(CacheHandler):
