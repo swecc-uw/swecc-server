2b0f25eee2faa008a227ad2517a23feab89dde32
randolph <y031125k@uw.edu>
2025-01-26 22:33:53 -0800

    capitalize model name and track component by creator

--- server/contentManage/models.py
+++ server/contentManage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from members.models import User
 
-class page(models.Model):
+class Page(models.Model):
     page_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -19,31 +19,31 @@ class page(models.Model):
     def __str__(self):
         return f"{self.title} - {self.created_by} - {self.last_updated}"
     
-class component(models.Model):
+class Component(models.Model):
     component_id = models.AutoField(primary_key=True)
-    parent_page = models.ForeignKey(page, on_delete=models.CASCADE)
+    parent_page = models.ForeignKey(Page, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
     class Meta:
-        indexes = [models.Index(fields=["-created"])]
+        indexes = [models.Index(fields=["-created_by"])]
 
     def __str__(self):
         return f"{self.title} - {self.created} - {self.created_by}"
 
 
-class image(models.Model):
+class Image(models.Model):
     image_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=25, null=False, blank=False)
-    parent_component = models.ForeignKey(component, on_delete=models.CASCADE, related_name="image_field")
+    parent_component = models.ForeignKey(Component, on_delete=models.CASCADE, related_name="image_field")
     image = models.ImageField(upload_to='components/images/')
 
     def __str__(self):
         return f"{self.image_id}: {self.title} - {self.image.url}"
     
-class text(models.Model):
-    parent_component = models.ForeignKey(component, on_delete=models.CASCADE, related_name="text_field")
+class Text(models.Model):
+    parent_component = models.ForeignKey(Component, on_delete=models.CASCADE, related_name="text_field")
     title = models.CharField(max_length=25, blank=False)
     short_line = models.CharField(max_length=25, blank=True, null=True)
     pargraph = models.TextField(blank=True, null=True)
