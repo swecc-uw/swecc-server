f88b74284e47c1c3c16e4bc678f17d5233711989
Advay Patil <advaypatil27@gmail.com>
2025-02-07 21:43:04 -0800

    Move cohort stats views to engagement

--- server/cohort/urls.py
+++ server/cohort/urls.py
@@ -8,29 +8,4 @@ urlpatterns = [
         views.CohortRetrieveUpdateDestroyView.as_view(),
         name="cohort-detail",
     ),
-    path(
-        "oa/<int:amt>",
-        view=views.UpdateOAStatsView.as_view(),
-        name="cohort-update-oa-stats",
-    ),
-    path(
-        "apply/<int:amt>",
-        view=views.UpdateApplicationStatsView.as_view(),
-        name="cohort-update-application-stats",
-    ),
-    path(
-        "interview/<int:amt>",
-        view=views.UpdateInterviewStatsView.as_view(),
-        name="cohort-update-interview-stats",
-    ),
-    path(
-        "offer/<int:amt>",
-        view=views.UpdateOffersStatsView.as_view(),
-        name="cohort-update-offer-stats",
-    ),
-    path(
-        "dailycheck",
-        view=views.UpdateDailyChecksView.as_view(),
-        name="cohort-update-daily-check",
-    ),
 ]
