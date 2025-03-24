3d0365365c69c423379ed7082b970cb7f87462b3
Advay Patil <advaypatil27@gmail.com>
2025-03-23 23:26:22 -0700

    Better singleton management

--- server/aws/s3.py
+++ server/aws/s3.py
@@ -5,7 +5,6 @@ import boto3
 class S3Client:
     instance = None
 
-    # Don't use outside of this class
     def __init__(self):
         self.access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", None)
         self.secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
@@ -19,10 +18,10 @@ class S3Client:
             aws_secret_access_key=self.secret_access_key,
         )
 
-    @classmethod
-    def get_instance(cls):
-        if not cls.instance:
-            cls.instance = S3Client()
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance = super(S3Client, cls).__new__(cls)
+            cls.instance.__init__()
         return cls.instance
 
     def get_presigned_url(self, bucket, key, expiration=3600):
