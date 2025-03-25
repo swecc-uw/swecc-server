ac9f316e696e16411af6686b22a474f905016f05
Elijah Melton <elimelt@uw.edu>
2025-03-25 14:59:13 -0700

    server side registration flow with auto reset pw

--- server/custom_auth/urls.py
+++ server/custom_auth/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('session/', views.SessionView.as_view(), name='api-session'),  
     path('whoami/', views.WhoAmIView.as_view(), name='api-whoami'), 
     path('register/', views.register_view, name='register'),
+    path('register/ssflow/', views.RegisterWithApiKeyView.as_view(), name='register-ssflow'),
     path('password-reset-confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
     path('jwt/', views.CreateTokenView.as_view(), name='api-jwt'),
     # path('<int:id>/verified/', views.DiscordVerificationView.as_view(), name='api-verified'),
