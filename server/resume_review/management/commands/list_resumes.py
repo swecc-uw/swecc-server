86a831e7c66e40ae80c0584be7a0431f362b1616
Advay Patil <advaypatil27@gmail.com>
2025-03-26 15:13:46 -0700

    Update help message for command

--- server/resume_review/management/commands/list_resumes.py
+++ server/resume_review/management/commands/list_resumes.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Command to print out all users"
+    help = "Command to print out all resumes"
 
     def handle(self, *args, **options):
         pp = (
