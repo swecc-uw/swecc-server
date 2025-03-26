3589dc8f70e98f3d0aa2281f280ad15cae1d74fb
Advay Patil <advaypatil27@gmail.com>
2025-03-25 17:50:36 -0700

    Setup deployment via Dockerfile

--- server/rabbitmq_listener.py
+++ server/rabbitmq_listener.py
@@ -13,7 +13,6 @@ RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 def callback(ch, method, properties, body):
     data = json.loads(body)
     print("Received:", data)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
@@ -21,7 +20,7 @@ channel = connection.channel()
 
 queue_name = "reviewed-feedback"
 channel.queue_declare(queue=queue_name, durable=True)
-channel.basic_consume(queue=queue_name, on_message_callback=callback)
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 print("Listening for messages...")
 channel.start_consuming()
