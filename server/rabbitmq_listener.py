2490d3569d788d2915af49b36982356f9b05db5c
Advay Patil <advaypatil27@gmail.com>
2025-03-26 15:11:06 -0700

    Complete RabbitMQ listener's functionality

--- server/rabbitmq_listener.py
+++ server/rabbitmq_listener.py
@@ -1,19 +1,60 @@
 import os
+
 import django
-import pika
-import json
 
 # Set up Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django.setup()
+
+import pika
+import json
+import logging
+from resume_review.models import Resume
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq-host")
 
 
 # Sample callback, will fill in later
 def callback(ch, method, properties, body):
-    data = json.loads(body)
-    print("Received:", data)
+    expected_fields = ["user_id", "resume_id", "feedback", "error"]
+
+    # Validate body
+    try:
+        data = json.loads(body)
+        for field in expected_fields:
+            if field not in data:
+                raise ValueError(f"Field {field} not found in message")
+    except json.JSONDecodeError as e:
+        logging.error(f"Error decoding JSON: {e}")
+        return
+    except ValueError as e:
+        logging.error(f"Error validating message: {e}")
+        return
+
+    if data["error"]:
+        logging.info(f"Received error message: {data['error']}")
+        return
+
+    resume_object = Resume.objects.filter(id=data["resume_id"]).first()
+
+    if not resume_object:
+        logging.error(
+            f"Resume with ID {data['resume_id']} not found"
+        )  # Technically unreachable, though can't hurt to check for
+        return
+
+    resume_object.feedback = data["feedback"]
+    resume_object.save()
+
+    logging.info(
+        f"Feedback for resume {data['resume_id']} updated with value {data['feedback']}"
+    )
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
@@ -23,5 +64,5 @@ queue_name = "reviewed-feedback"
 channel.queue_declare(queue=queue_name, durable=True)
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-print("Listening for messages...")
+logging.info("Listening for messages...")
 channel.start_consuming()
