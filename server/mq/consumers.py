437c8e221814409b4a567b19e3798ba5fcfd0371
Advay Patil <advaypatil27@gmail.com>
2025-04-24 11:09:43 -0700

    Lint

--- server/mq/consumers.py
+++ server/mq/consumers.py
@@ -2,9 +2,9 @@ import json
 import logging
 
 import mq
+from asgiref.sync import sync_to_async
 from pika import BasicProperties
 from resume_review.models import Resume
-from asgiref.sync import sync_to_async
 
 logger = logging.getLogger(__name__)
 
@@ -21,11 +21,7 @@ async def verified_email_callback(
     logger.info(f"Received verified email message: {body}")
 
 
-@mq.consumer(
-    queue="server.reviewed-resume",
-    exchange='ai',
-    routing_key='reviewed'
-)
+@mq.consumer(queue="server.reviewed-resume", exchange="ai", routing_key="reviewed")
 async def reviewed_feedback(
     body: bytes,
     properties: BasicProperties,
@@ -39,24 +35,22 @@ async def reviewed_feedback(
     if feedback is None or key is None:
         logger.error("Feedback or key not found in message")
         return
-    
+
     user_id, resume_id, file_name = key.split("-")
 
     @sync_to_async
     def perform_database_operations():
         resume_object = Resume.objects.filter(id=int(resume_id)).first()
         if not resume_object:
-            logger.error(
-                f"Resume with ID {resume_id} not found"
-            )
+            logger.error(f"Resume with ID {resume_id} not found")
             return False
-        
+
         if resume_object.member.id != int(user_id):
             logger.error(
                 f"Resume with ID {resume_id} does not belong to user {user_id}"
             )
             return False
-        
+
         if resume_object.file_name != file_name:
             logger.error(
                 f"Resume with ID {resume_id} does not have file name {file_name}"
@@ -68,7 +62,4 @@ async def reviewed_feedback(
 
     successful = await perform_database_operations()
     if successful:
-        logger.info(
-            f"Feedback for resume {resume_id} updated with value {feedback}"
-        )
-
+        logger.info(f"Feedback for resume {resume_id} updated with value {feedback}")
