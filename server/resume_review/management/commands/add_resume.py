6a13ec9fafc32c839345000a69cefc54f274c8f3
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2025-05-03 13:06:03 -0700

    added save resume feature

--- server/resume_review/management/commands/add_resume.py
+++ server/resume_review/management/commands/add_resume.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandParser
-from resume_review.models import Resume
-from members.models import User
 from django.db import IntegrityError
+from members.models import User
+from resume_review.models import Resume
+
 
 class Command(BaseCommand):
     help = "Command to add resumes"
@@ -12,25 +13,42 @@ class Command(BaseCommand):
         file_name = options.get("file_name")
         file_size = options.get("file_size")
 
-        if feedback is None or  username is None or file_name is None or file_size is None:
+        if (
+            feedback is None
+            or username is None
+            or file_name is None
+            or file_size is None
+        ):
             self.stdout.write("Error processing parameters")
             return
-        member = User.objects.filter(username = username).first()
+        member = User.objects.filter(username=username).first()
         if member is None:
-            self.stdout.write("User not found") 
+            self.stdout.write("User not found")
             return
         try:
-            resume = Resume.objects.create(member=member,feedback=feedback,file_name=file_name,file_size=file_size)
+            resume = Resume.objects.create(
+                member=member,
+                feedback=feedback,
+                file_name=file_name,
+                file_size=file_size,
+            )
+            resume.save()
             self.stdout.write("Successfully created resume")
         except IntegrityError:
             self.stdout.write("Duplicate file name found")
-        
-        
 
-        
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--feedback", type=str,help="The feedback on this particular resume")
-        parser.add_argument("--username", type=str,help="The username associated with this particular resume")
-        parser.add_argument("--file_name", type=str,help="The file name of this particular resume")
-        parser.add_argument("--file_size",type=int,help="The file size of this particular resume")
-
+        parser.add_argument(
+            "--feedback", type=str, help="The feedback on this particular resume"
+        )
+        parser.add_argument(
+            "--username",
+            type=str,
+            help="The username associated with this particular resume",
+        )
+        parser.add_argument(
+            "--file_name", type=str, help="The file name of this particular resume"
+        )
+        parser.add_argument(
+            "--file_size", type=int, help="The file size of this particular resume"
+        )
